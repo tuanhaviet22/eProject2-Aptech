30944b4edd61d58ef10b1ce80bdd7e1ba6c9be67
tuanha <tuanhaviet22@gmail.com>
2021-03-06 23:53:30 +0700

    builder

--- build/generated-sources/ap-source-output/Models/Authors_.java
+++ build/generated-sources/ap-source-output/Models/Authors_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-06T23:26:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-06T23:53:16")
 @StaticMetamodel(Authors.class)
 public class Authors_ { 
 
